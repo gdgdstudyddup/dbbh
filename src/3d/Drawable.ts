cf25308ab98ced6b55a18c8fccef83af5d32aac4
tianlu <tianlu@qunhemail.com>
2023-05-05 17:57:47 +0800

    stash

--- src/3d/Drawable.ts
+++ src/3d/Drawable.ts
@@ -1,7 +1,15 @@
+import { Geometry } from "./geometry/geometry";
+import { Material } from "./material/Material";
 import { Object3D } from "./Object3D";
 
 class Drawable extends Object3D {
-    constructor() {
+    geometry: Geometry;
+    material: Material;
+    constructor(geometry: Geometry, material: Material | Material[]) {
         super();
+    }
+
+    convertToCluster(){
+        
     }
 }
--- src/3d/material/Material.ts
+++ src/3d/material/Material.ts
@@ -1,3 +1,3 @@
 export class Material {
-
+    range = [0, +Infinity];
 }
